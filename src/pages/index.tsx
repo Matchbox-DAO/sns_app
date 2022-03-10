ee24866c72add71d31940403473ff49ce84e7597
dhruvkelawala <dhruvrk2000@gmail.com>
2022-03-10 12:40:15 +0530

    add eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,32 @@ import {
   useStarknetCall,
   useStarknetInvoke,
   useStarknetTransactionManager,
-  Transaction
+  Transaction,
 } from '@starknet-react/core'
 import { BigNumber } from 'bignumber.js'
 import type { NextPage } from 'next'
 import { ConnectWallet } from '~/components/ConnectWallet'
 import { useSNSContract } from '~/hooks/sns'
 import { TransactionList } from '~/components/TransactionList'
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 
 const Home: NextPage = () => {
-
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
   const { account } = useStarknet()
   const { contract: snsContract } = useSNSContract()
   // const { invoke: invokeSnsRegister } = useStarknetInvoke({ contract: snsContract, method: 'sns_register' })
-  const { data, loading, error, reset, invoke:invokeSnsRegister } = useStarknetInvoke({
+  const {
+    data,
+    loading,
+    error,
+    reset,
+    invoke: invokeSnsRegister,
+  } = useStarknetInvoke({
     contract: snsContract,
     method: 'sns_register',
   })
@@ -26,13 +36,11 @@ const Home: NextPage = () => {
   const onSubmit = (data: any) => {
     if (!account) {
       console.log('user wallet not connected yet.')
-    }
-    else if (!snsContract) {
+    } else if (!snsContract) {
       console.log('frontend not connected to SNS contract')
-    }
-    else {
-      let data_dec_str = string_to_felt_bn (data['nameRequired']).toString()
-      invokeSnsRegister ({ args: { name: data_dec_str } })
+    } else {
+      let data_dec_str = string_to_felt_bn(data['nameRequired']).toString()
+      invokeSnsRegister({ args: { name: data_dec_str } })
       console.log('invoked sns_register() with ', data_dec_str)
     }
   }
@@ -52,7 +60,7 @@ const Home: NextPage = () => {
         {/* include validation with required or other standard HTML validation rules */}
         {/* errors will return when field validation fails  */}
 
-        <input defaultValue="name to register" {...register("nameRequired", { required: true })} />
+        <input defaultValue="name to register" {...register('nameRequired', { required: true })} />
         {errors.nameRequired && <span> (This field is required) </span>}
 
         <input type="submit" />
@@ -64,7 +72,6 @@ const Home: NextPage = () => {
       </div>
 
       <DemoTransactionManager />
-
     </div>
   )
 }
@@ -78,24 +85,14 @@ function DemoTransactionManager() {
         {transactions.length === 0
           ? 'No transactions'
           : transactions.map((tx, index) => (
-              <TransactionItem
-                key={index}
-                transaction={tx}
-                onClick={() => removeTransaction(tx.transactionHash)}
-              />
+              <TransactionItem key={index} transaction={tx} onClick={() => removeTransaction(tx.transactionHash)} />
             ))}
       </div>
     </div>
   )
 }
 
-function TransactionItem({
-  transaction,
-  onClick,
-}: {
-  transaction: Transaction
-  onClick: () => void
-}) {
+function TransactionItem({ transaction, onClick }: { transaction: Transaction; onClick: () => void }) {
   return (
     <div>
       {transaction.status}: {transaction.transactionHash} <button onClick={onClick}>remove</button>
@@ -103,11 +100,13 @@ function TransactionItem({
   )
 }
 
-function string_to_felt_bn (str : string){
+function string_to_felt_bn(str: string) {
   BigNumber.config({ EXPONENTIAL_AT: 76 })
 
-  let array = str.split ('').map (function (c : string) { return c.charCodeAt (0); })
-  let felt_bn = new BigNumber (0)
+  let array = str.split('').map(function (c: string) {
+    return c.charCodeAt(0)
+  })
+  let felt_bn = new BigNumber(0)
 
   for (const e of array) {
     felt_bn = felt_bn.multipliedBy(256)
