24b2990ac1d0329c63d724434b4b87fba6543a88
dhruvkelawala <dhruvrk2000@gmail.com>
2022-03-12 23:47:26 +0530

    update font import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
           <link
             href="https://fonts.googleapis.com/css2?family=Overpass:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
